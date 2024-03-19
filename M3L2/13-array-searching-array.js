55e6fc0c190b1aae260357580ca4c7b62e874941
Martin <martin_lenart@icloud.com>
2024-03-19 11:03:25 +0100

    update

--- M3L2/13-array-searching-array.js
+++ M3L2/13-array-searching-array.js
@@ -1,7 +1,7 @@
 'use strict';
 import {seedGenerator, uniqueId, randomNumber, deepCopy, isEqual} from '../SeidoHelpers/seido-helpers.js';
 
-console.group('Searching with exact eqaulity');
+console.group('Searching with exact equality');
 const animals = ['dog', 'cat', 'seal', 'elephant', 'walrus', 'lion'];
 console.log(`Elephant is at ${animals.indexOf('elephant')}`); // 3
 console.log(`Walrus is at ${animals.lastIndexOf('walrus')}`); // 4
@@ -10,9 +10,15 @@ console.groupEnd();
 
 console.group('Searching with filter criteria');
 const nums = [2, 4, 19, 15, 183, 6, 7, 1, 1];
+
 // Find the first value over 10.
 const bigNum = nums.find(element => element > 10);
 console.log(`First >10 number is ${bigNum}`); // 19 (the first match)
+
+// Find the first value over 10.
+const bigNums = nums.filter(element => element > 10);
+console.log(`Array >10 number is ${bigNums}`); // all matches
+
 
 const bigNumIndex = nums.findIndex(element => element > 100);
 console.log(`First >10 number is at index ${bigNumIndex}`); // 4
