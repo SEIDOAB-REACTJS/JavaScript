f5786de3fce400e882c356e588d20d62fca7a4b9
Martin <martin_lenart@icloud.com>
2024-05-16 11:58:12 +0200

    update

--- M3L2/play07.js
+++ M3L2/play07.js
@@ -1,41 +1,12 @@
 'use strict';
 import {seedGenerator, uniqueId, randomNumber, deepCopy} from '../SeidoHelpers/seido-helpers.js';
+import {Book, LibraryService} from './play07-book.js';
 
-export function Book (title, author, millionSold, publishYear){
-
-    this.bookId = uniqueId();
-    this.title = title;
-    this.author = author;
-    this.millionsSold = millionSold;
-    this.publishedYear = publishYear;
-
-    this.toString = function () {
-        return `${this.title} written by ${this.author} in the year ${this.publishedYear}. ${this.millionsSold} million copies sold`;}
-
-    this.seed = function (_seeder) {
-
-        this.title = _seeder.latinSentence;
-        this.author = _seeder.fullName;
-        this.millionsSold = randomNumber(1, 100);
-        this.publishedYear = randomNumber(1800, 2020);
-        return this;
-    }
-
-    this.seedMany = function (nrItems, _seeder) {
-        let books = [];
-        for (let index = 0; index < nrItems; index++) {
-
-            const b = new Book().seed(_seeder);
-            books.push(b);
-        };
-        return books;
-    }
-}
-
-const b = new Book("Huckleberry Finn", "Mark Tawin", 100, 1875);
+/*
+const b = new Book("Huckleberry Finn", "Mark Tawin", "Adventure", 100, 1875);
 console.log(b.bookId, b.toString());
 
-const b1 = new Book("Frankenstein", "Mary Ann Shelly", 200, 1888);
+const b1 = new Book("Frankenstein", "Mary Ann Shelly", "Horror", 200, 1888);
 console.log(b1.bookId, b1.toString());
 
 const _seeder = new seedGenerator();
@@ -44,3 +15,22 @@ console.log(b2.bookId, b2.toString());
 
 const books = new Book().seedMany(100, _seeder)
 console.log(books);
+*/
+
+
+
+
+//your webbpage
+const library = new LibraryService();
+console.log ("from my library");
+console.log("Total nr books", library.info());
+console.log("Adventure books", library.info("Horror"));
+
+let p1 = library.readBooks(0, 50);
+console.log("Books", p1);
+console.log("Books", library.readBooks(1, 50));
+console.log("Books", library.readBooks(2, 50));
+
+let bookdetail = library.readBook(p1.pageItems[0].bookId);
+console.log(bookdetail);
+
