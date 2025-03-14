563c3771bffe3c96e11aebc5ed32dbc8ff80da4e
Martin <martin_lenart@icloud.com>
2025-03-14 09:41:47 +0100

    updated play1

--- Playground/Play1/index.js
+++ Playground/Play1/index.js
@@ -3,7 +3,7 @@
 let myObj = {name: "Martin", phone: "123 12345"};
 myObj.city = "Stockholm";
 
-let pTag = document.getElementById("greeting");
+let pTag = document.querySelector("#greeting");
 pTag.innerHTML = `<h1>Hello ${myObj.name}</h1><h2>Phonenr: ${myObj.phone}</h2>`;
 
 let phoneTag = document.createElement("h3");
@@ -12,7 +12,7 @@ pTag.appendChild(phoneTag);
 
 
 let persons = ["Martin", "Christoffer", "Alexandra", "Jacob", "Alfons"];
-let ulTag = document.getElementById("namelist");
+let ulTag = document.querySelector("#namelist");
 
 for (const element of persons) {
     
@@ -20,5 +20,14 @@ for (const element of persons) {
     liTag.innerText = element;
     ulTag.appendChild(liTag);
 }
+
+
+const pTag1 = document.querySelector('p');
+const pTags = document.querySelectorAll('main p');
+
+for (const tag of pTags) {
+    tag.classList.add("bg-color");
+}
+
 
 console.log(myObj);
