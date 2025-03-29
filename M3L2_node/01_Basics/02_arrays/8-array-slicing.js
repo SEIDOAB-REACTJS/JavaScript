e8cde436dfa2ab5a3c27d49bafaedfb8108dad3b
Martin <martin_lenart@icloud.com>
2025-03-29 12:47:07 +0100

    update

---
+++ M3L2_node/01_Basics/02_arrays/8-array-slicing.js
@@ -0,0 +1,31 @@
+'use strict';
+import {seedGenerator, uniqueId, randomNumber, deepCopy, isEqual} from '../../../SeidoHelpers/seido-helpers.js';
+
+const animals = ['elephant', 'tiger', 'emu', 'zebra', 'cat', 'dog', 'rabbit', 'eel', 'goose', 'earwig'];
+console.log(`Original array: ${animals}`);
+
+
+let pagesize = 2
+let pageNr = 4
+let page = animals.slice(pageNr * pagesize, pageNr * pagesize + pagesize);
+
+// Copy by position
+// Get the chunk from index 4 to index 7.
+const domestic = animals.slice(4, 7);
+console.log(`Domestic: ${domestic}`);   // ['cat', 'dog', 'rabbit'];
+
+// Put a new animal in the middle using ... and slice operator
+const firstHalf = animals.slice(0, 4);
+const secondHalf = animals.slice(4, animals.length);
+
+const extraAnimals = [...firstHalf, 'platypus', ...secondHalf];
+console.log(`Extra: ${extraAnimals}`);
+
+// Copy by filter criteria
+const animalsE = animals.filter(animal => animal[0].toLowerCase() === 'e');
+console.log(`E letter: ${animalsE}`);
+
+/* Exercise
+1. use the ...operator and array.slice to create a new deck of cards which contains
+   ONLY the first 10 and last 5 cards of the original deck
+*/
---
+++ M3L4_dom/readme.txt
@@ -0,0 +1,4 @@
+//to import ES6 modules you have to have a package.json file in you directory.
+//in terminal type npm init -y
+//in the created package.json add on first row
+//"type": "module",
