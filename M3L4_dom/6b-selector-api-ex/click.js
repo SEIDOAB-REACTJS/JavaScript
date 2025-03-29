ce4585d0b7f70915804be847f48e1a5f87fc6bee
Martin <martin_lenart@icloud.com>
2025-03-29 14:02:35 +0100

    update

--- M3L4_dom/6b-selector-api-ex/click.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-import {seedGenerator, uniqueId, randomNumber, deepCopy, isEqual} from '../../SeidoHelpers/seido-helpers.js';
-
-//https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
-//https://developer.mozilla.org/en-US/docs/Web/API/Document_object_model/Locating_DOM_elements_using_selectors
-
